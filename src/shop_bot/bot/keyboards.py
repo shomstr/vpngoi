00d580e2493a8c9a5bb2b1c6358530629d5dd278
shomstr <schamil.gab@yandex.ru>
2026-01-02 03:19:30 +0300

    kd

--- src/shop_bot/bot/keyboards.py
+++ src/shop_bot/bot/keyboards.py
@@ -79,7 +79,7 @@ def create_host_selection_keyboard(hosts: list, action: str) -> InlineKeyboardMa
     for host in hosts:
         callback_data = f"select_host_{action}_{host['host_name']}"
         builder.button(text=host['host_name'], callback_data='d')
-    builder.button(text="🌐 Подписка на все серверы", callback_data="get_full_subscription")
+    builder.button(text="🌐 Подписка на все серверы", callback_data="buy_full_subscription")
     builder.button(text="⬅️ Назад", callback_data="manage_keys" if action == 'new' else "back_to_main_menu")
     builder.adjust(1)
     return builder.as_markup()
